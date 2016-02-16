eb365abdfd85211932f4eb38e41135b0744957e4
Vinicius <m.vini846@gmail.com>
2016-02-16 16:55:04 -0300

    validar codigo

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -4,23 +4,23 @@ angular.module("listaTele").factory('Contatos',	['$resource', function($resource
 	return $resource('', {}, {
 		todos : {
 			method : 'GET',
-			url : 'http://localhost:8080/integration/index/todos',
+			url : 'http://localhost:9090/integration/index/todos',
 			isArray : true
 		},
 		salvar : {
 			method : 'POST',
-			url : 'http://localhost:8080/integration/index/salvar/:fluxoparams',
+			url : 'http://localhost:9090/integration/index/salvar/:fluxoparams',
 			params: {fluxoparams: '@fluxo'},
 			isArray : true
 		},
 		fluxoSelected : {
 			method : 'POST',
-			url : 'http://localhost:8080/integration/index/exibir/:codigoFluxo',
+			url : 'http://localhost:9090/integration/index/exibir/:codigoFluxo',
 			params: {codigoFluxo: '@cod'},
 		},
 		excluir : {
 			method : 'POST',
-			url : 'http://localhost:8080/integration/index/excluir/:fluxocod',
+			url : 'http://localhost:9090/integration/index/excluir/:fluxocod',
 			params : {fluxocod: '@codigoflux'},
 			isArray : true
 		}
@@ -82,9 +82,22 @@ angular.module("listaTele").controller("listaCtrl", function($scope, Contatos) {
 	}
 
 	$scope.salvar = function(fc) {
-		Contatos.salvar({fluxo: fc}, function(data) {
-			$scope.fluxos = angular.fromJson(data);
-		});
+		if(validaCodigo(fc)) {
+			Contatos.salvar({fluxo: fc}, function(data) {
+				$scope.fluxos = angular.fromJson(data);
+			});
+		}
+	};
+	
+	var validaCodigo = function(fc) {
+		var todosFluxos = $scope.fluxos;
+		for(var i=0; i<=todosFluxos.length-1;i++){
+			if(fc.codigoFluxo.toString() == todosFluxos[i].codigoFluxo.toString()) {
+				$scope.validacao = true;
+				return false; 
+			}
+		}
+		return true;
 	};
 
 	var exibirEdicao = function(codFluxo, confirm) {
